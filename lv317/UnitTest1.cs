e683f6f141171c276e6b1b5cb013c5e1be941f4f
garasym <garasym@gmail.com>
2018-06-04 13:52:22 +0300

    Template for Tests by NUnit

--- lv317/UnitTest1.cs
+++ lv317/UnitTest1.cs
@@ -1,14 +1,77 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace lv317
 {
-    [TestClass]
+    // Template for Tests
+    //[TestClass]
+    [TestFixture]
+    //[Parallelizable(ParallelScope.All)]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
         {
+            Console.WriteLine("[OneTimeSetUp] BeforeAllMethods()");
+            //MessageBox.Show("[OneTimeSetUp] BeforeAllMethods()", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            Console.WriteLine("[OneTimeTearDown] AfterAllMethods()");
+            //MessageBox.Show("[OneTimeTearDown] AfterAllMethods()", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("[SetUp] SetUp()");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("[TearDown] TearDown()");
+        }
+
+        //[TestMethod]
+        [Test]
+        public void TestMethod1(
+            [Values("Hello", "World")]
+            string s,
+            [Random(1,10,5)]
+            int x)
+        {
+            Console.WriteLine("Ok, s=" + s + " x=" + x);
+        }
+
+        [TestCase(5, ExpectedResult = true)]
+        //[TestCase(-15, ExpectedResult = false)]
+        public bool TestMethod2(int x)
+        {
+            return x > 0;
+        }
+
+        // DataProvider
+        private static readonly object[] ConverterData =
+        {
+            new object[] { 65, 'A' },
+            new object[] { 97, 'a' },
+            new object[] { 98, 'b' }
+        };
+
+        //[TestCase(65, 'A')]
+        //[TestCase(97, 'a')]
+        [Test, TestCaseSource(nameof(ConverterData))]
+        public void TestMethod3(int x, char c)
+        {
+            char expexted = c;
+            char actual = (char)x;
+            Assert.AreEqual(expexted, actual);
+        }
+
     }
 }
