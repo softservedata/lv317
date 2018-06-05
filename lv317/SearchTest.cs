ebb8786d82a5eaab861d337ea10d3ca0d74b9f12
garasym <garasym@gmail.com>
2018-06-05 14:22:22 +0300

    Example Search

---
+++ lv317/SearchTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+//using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using lv317.Tools;
+
+namespace lv317
+{
+    [TestFixture]
+    public class SearchTest : TestRunner
+    {
+        // DataProvider
+        private static readonly object[] Items =
+        {
+            new object[] { "iPhone", "iPhone is a revolutionary new mobile phone that allows you to make a call by simply tapping" },
+            new object[] { "Apple iPhone SE 64GB", "IPS, 1136x640) / Apple A9 /" }
+        };
+
+        [Test, TestCaseSource(nameof(Items))]
+        public void SearchItem(string itemName, string description)
+        {
+            driver.FindElement(By.Name("search")).Click();
+            driver.FindElement(By.Name("search")).Clear();
+            driver.FindElement(By.Name("search")).SendKeys(itemName);
+            Thread.Sleep(1000);
+            //
+            driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg")).Click();
+            Thread.Sleep(1000);
+            //
+            IWebElement webDescription = driver.FindElement(By.XPath("//h4/a[text()='" + itemName + "']/../following-sibling::p[not(@*)]"));
+            Assert.True(webDescription.Text.Contains(description));
+            Thread.Sleep(2000);
+        }
+    }
+
+}
---
+++ lv317/Tools/TestRunner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+//using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+namespace lv317.Tools
+{
+    [TestFixture]
+    public abstract class TestRunner
+    {
+        private readonly int IMPLICIT_WAIT = 10;
+        protected string urlUnderTest = "http://atqc-shop.epizy.com";
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(IMPLICIT_WAIT);
+            //
+            Console.WriteLine("[OneTimeSetUp] BeforeAllMethods()");
+            //MessageBox.Show("[OneTimeSetUp] BeforeAllMethods()", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+            Console.WriteLine("[OneTimeTearDown] AfterAllMethods()");
+            //MessageBox.Show("[OneTimeTearDown] AfterAllMethods()", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl(urlUnderTest);
+            Console.WriteLine("[SetUp] SetUp()");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // TODO logout
+            Console.WriteLine("[TearDown] TearDown()");
+        }
+
+    }
+}
